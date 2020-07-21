f10f49b6e65addd3d321c23a11ecf563b6485059
hoschiho <pascal.hoschi@gmail.com>
2020-07-21 23:35:44 +0200

    added meeh.

--- lib/components/lustigi_radio_farbe.dart
+++ lib/components/lustigi_radio_farbe.dart
@@ -12,13 +12,17 @@ class CustomRadioState extends State<CustomRadio> {
 
   @override
   void initState() {
-    // TODO: meeeh
     super.initState();
-    sampleData.add(new RadioModel(false,Colors.lightGreenAccent));
-    sampleData.add(new RadioModel(false,Colors.tealAccent));
-    sampleData.add(new RadioModel(false,Colors.cyanAccent));
 
-    sampleData.add(new RadioModel(false,Colors.redAccent));
+    sampleData.add(new RadioModel(false,Color(0xFFE5554F)));
+    sampleData.add(new RadioModel(false,Color(0xFFFF9E1B)));
+    sampleData.add(new RadioModel(false,Color(0xFFFFD700)));
+    sampleData.add(new RadioModel(false,Color(0xFF79BE70)));
+    sampleData.add(new RadioModel(false,Color(0xFF279989)));
+    sampleData.add(new RadioModel(false,Color(0xFF00A7B5)));
+    sampleData.add(new RadioModel(false,Color(0xFFB884CB)));
+    sampleData.add(new RadioModel(false,Color(0xFFEF95CF)));
+    sampleData.add(new RadioModel(false,Color(0xFFF1BDC8)));
   }
 
   @override
@@ -27,17 +31,16 @@ class CustomRadioState extends State<CustomRadio> {
       height: 100,
 
       child: ListView.builder(
-
         scrollDirection: Axis.horizontal,
         itemCount: sampleData.length,
         itemBuilder: (BuildContext context, int index) {
-
 
           return new RadioItem(
               sampleData[index],
               (){
                 setState(() {
-                  sampleData.forEach((element) => element.isSelected = false);    sampleData[index].isSelected = true;
+                  sampleData.forEach((element) => element.isSelected = false);
+                  sampleData[index].isSelected = true;
                 });
           }
 
@@ -51,27 +54,21 @@ class CustomRadioState extends State<CustomRadio> {
 
 class RadioItem extends StatelessWidget {
   final RadioModel _item;
-
   final Function onTap;
-
-
   RadioItem(this._item, this.onTap);
   @override
   Widget build(BuildContext context) {
 
-
-
-
     return new Container(
       height: 50.0,
       width: 50.0,
-      margin: new EdgeInsets.all(15.0),
+      margin: new EdgeInsets.all(13.0),
       child: new Center(
         child: InkWell(
           onTap: onTap,
           child: Container(
-              height: _item.isSelected ? 35.0 : 50.0,
-              width: _item.isSelected ? 35.0 : 50.0,
+              height: _item.isSelected ? 40.0 : 50.0,
+              width: _item.isSelected ? 40.0 : 50.0,
               decoration: new BoxDecoration(
                 color: _item.color,
                 borderRadius: const BorderRadius.all(const Radius.circular(5.0)
