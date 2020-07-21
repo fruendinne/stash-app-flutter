c8550902dc186ad8a45edf0dfeca5f2dce36ecd7
lucafluri <luca.fluri4@gmail.com>
2020-07-21 16:44:29 +0200

    center thumbs button

--- lib/screens/details/details.dart
+++ lib/screens/details/details.dart
@@ -17,33 +17,60 @@ class DetailsScreenView
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      Container(
-        color: Colors.lime,
-        child: Column(children: <Widget>[
-        Row(children: <Widget>[
-          Text("Stash", style: TextStyle(fontSize: 50, color: Colors.white)),
-          Spacer(),
-          Row(children: <Widget>[
-            Text("16", style: TextStyle(fontSize: 24, color: Colors.white)),
-            Text("×", style: TextStyle(fontSize: 24, color: Colors.white)),
-            Icon(MdiIcons.glasses, color: Colors.white,)
-          ],)
-        ],),
-        Container(child: Image.network(
-            "https://img.blick.ch/incoming/15824173-v1-tiger-king-joe-exotic-netflix.jpg?imwidth=1200&ratio=1_1&x=151&y=0&width=720&height=720"),
-            height: 200,
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Colors.lime,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Text("Stash",
+                      style: TextStyle(fontSize: 50, color: Colors.white)),
+                  Spacer(),
+                  Row(
+                    children: <Widget>[
+                      Text("16",
+                          style: TextStyle(fontSize: 24, color: Colors.white)),
+                      Text("×",
+                          style: TextStyle(fontSize: 24, color: Colors.white)),
+                      Icon(
+                        MdiIcons.glasses,
+                        color: Colors.white,
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                child: Image.network(
+                    "https://img.blick.ch/incoming/15824173-v1-tiger-king-joe-exotic-netflix.jpg?imwidth=1200&ratio=1_1&x=151&y=0&width=720&height=720"),
+                height: 200,
+              ),
+            ],
+          ),
         ),
-      ],),),
-      Container(
-          color: Colors.white,
-          child: Column(children: <Widget>[
-        TextField(),
-        Row(mainAxisAlignment: MainAxisAlignment.center,children: <Widget>[
-          IconButton(icon: Icon(MdiIcons.thumbUp), onPressed: (){},),
-          IconButton(icon: Icon(MdiIcons.thumbDown), onPressed: (){},),
-        ],),
-      ],)),
-    ],);
+        Container(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Text("ioboboosbidbgobiobgbue"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(MdiIcons.thumbUpOutline),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(MdiIcons.thumbDownOutline),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ],
+            )),
+      ],
+    );
   }
 }
